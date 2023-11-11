cf7d4e71b2d82a93a83db451d5b396ad63a97d64
dan000317 <dan000317@gmail.com>
2023-11-11 13:10:24 +0000

    revised the filepath

--- activity4/word-count.py
+++ activity4/word-count.py
@@ -3,7 +3,7 @@ from pyspark import SparkConf, SparkContext
 conf = SparkConf().setMaster("local").setAppName("WordCount")
 sc = SparkContext(conf = conf)
 
-input = sc.textFile("file:///sparkcourse/book.txt")
+input = sc.textFile("/root/L.ApacheSpark/activity4/Book")
 words = input.flatMap(lambda x: x.split())
 wordCounts = words.countByValue()
 
